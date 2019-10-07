59e54a510362bf0d6d9ea6b7d7ae62e55570fa2e
Daumur <daumur@et.esiea.fr>
2019-10-07 12:10:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/menu/Data.java
@@ -0,0 +1,17 @@
+package com.example.menu;
+
+public class Data {
+
+    public static String [] title = new String[]{
+
+            "Panda Yin",
+            "Couscous tajin",
+            "Paella Loco"
+    };
+
+    public static int[] picturePath = new int[]{
+            R.drawable.ic_one,
+            R.drawable.ic_two,
+            R.drawable.ic_three
+    };
+}
